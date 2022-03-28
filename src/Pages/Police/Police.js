fa99c889a2089cc86a109b9c4ab1adc2b431aaf4
asher <greatasher7@gmail.com>
2022-03-29 01:18:19 +0900

    modify links

--- src/Pages/Police/Police.js
+++ src/Pages/Police/Police.js
@@ -1,53 +1,47 @@
 import DetailFrame from "Components/DetailFrame";
 
 const Police = () => {
-    
-    const data = {
-        name: "KNPA",
-        pageUrl: "https://greatasher7.github.io/police_agency_cloning/",
-        github: "https://github.com/greatasher7/police_agency_cloning",
-        screenshot: [
-            require("Assets/police/screenshot1.png"),
-            require("Assets/police/screenshot2.png"),
-            require("Assets/police/screenshot3.png")
-        ],
-        logo: require("Assets/police/policeLogo.png"),
-        skills: [
-            {name: "html", url: require("Assets/html.png")},
-            {name: "css", url: require("Assets/css.png")},
-            {name: "js", url: require("Assets/js.png")},
-            {name: "jquery", url: require("Assets/jquery.png")}
-        ],
-        description : [
-            "\"웹 표준에 맞게 제작하였으며, 이전 브라우저(IE8)를 고려하여 만들었습니다.", 
-            "제이쿼리를 사용한 동적 요소들과 로그인 페이지를 포함한 서브페이지를 구현했습니다.\""
-        ],
-        challenges : [
-            "Web Standard",
-            "Web Accessibility (skip menu /...)",
-            "CSS layout (Position / Float /...)",
-            "브라우저 호환성 (caniuse.com)",
-            "CSS 모듈화 -> 중복 제거",
-            "iframe 유튜브 영상",
-            "Kakao Map Api",
-        ],
-        browsers: [
-            "IE 8-11",
-            "Chrome",
-            "Firefox",
-            "..."
-        ],
-        period: "2021.02 - 2021.03", 
-    }
-    
-    const styles = {
-        shadowColor: "#f7f7f7",
-        bgColor: "#04264c",
-        fontColor1: "#e8b803",
-        fontColor2: "#cccccc",
-    }
+  const data = {
+    name: "KNPA",
+    pageUrl: "https://greatasher7.github.io/Police_Agency_Cloning/",
+    github: "https://github.com/greatasher7/Police_Agency_Cloning",
+    screenshot: [
+      require("Assets/police/screenshot1.png"),
+      require("Assets/police/screenshot2.png"),
+      require("Assets/police/screenshot3.png"),
+    ],
+    logo: require("Assets/police/policeLogo.png"),
+    skills: [
+      { name: "html", url: require("Assets/html.png") },
+      { name: "css", url: require("Assets/css.png") },
+      { name: "js", url: require("Assets/js.png") },
+      { name: "jquery", url: require("Assets/jquery.png") },
+    ],
+    description: [
+      '"웹 표준에 맞게 제작하였으며, 이전 브라우저(IE8)를 고려하여 만들었습니다.',
+      '제이쿼리를 사용한 동적 요소들과 로그인 페이지를 포함한 서브페이지를 구현했습니다."',
+    ],
+    challenges: [
+      "Web Standard",
+      "Web Accessibility (skip menu /...)",
+      "CSS layout (Position / Float /...)",
+      "브라우저 호환성 (caniuse.com)",
+      "CSS 모듈화 -> 중복 제거",
+      "iframe 유튜브 영상",
+      "Kakao Map Api",
+    ],
+    browsers: ["IE 8-11", "Chrome", "Firefox", "..."],
+    period: "2021.02 - 2021.03",
+  };
 
-    return <DetailFrame data = {data} styles = {styles}/>
-}
+  const styles = {
+    shadowColor: "#f7f7f7",
+    bgColor: "#04264c",
+    fontColor1: "#e8b803",
+    fontColor2: "#cccccc",
+  };
+
+  return <DetailFrame data={data} styles={styles} />;
+};
 
 export default Police;
--- src/Pages/Tada/Tada.js
+++ src/Pages/Tada/Tada.js
@@ -1,53 +1,46 @@
 import DetailFrame from "Components/DetailFrame";
 
 const Tada = () => {
-    
-    const data = {
-        name: "Tada",
-        pageUrl: "https://greatasher7.github.io/tada_responsive/",
-        github: "https://github.com/greatasher7/tada_responsive",
-        screenshot: [
-            require("Assets/tada/screenshot1.png"),
-            require("Assets/tada/screenshot2.png"),
-            require("Assets/tada/screenshot3.png")
-        ],
-        logo: require("Assets/tada/tadaLogo.png"),
-        skills: [
-            {name: "html", url: require("Assets/html.png")},
-            {name: "css", url: require("Assets/css.png")},
-            {name: "js", url: require("Assets/js.png")},
-            {name: "zeplin", url: require("Assets/zeplin.png")}
-        ],
-        description : [
-            "\"CSS 변수를 사용했으며, 반응형으로 구현했습니다.(PC/Tablet/Mobile).", 
-            "Vanilla.JS만을 사용하여 Javascript에 대한 이해도를 높였습니다.\"", 
+  const data = {
+    name: "Tada",
+    pageUrl: "https://greatasher7.github.io/Tada_Cloning/",
+    github: "https://github.com/greatasher7/Tada_Cloning",
+    screenshot: [
+      require("Assets/tada/screenshot1.png"),
+      require("Assets/tada/screenshot2.png"),
+      require("Assets/tada/screenshot3.png"),
+    ],
+    logo: require("Assets/tada/tadaLogo.png"),
+    skills: [
+      { name: "html", url: require("Assets/html.png") },
+      { name: "css", url: require("Assets/css.png") },
+      { name: "js", url: require("Assets/js.png") },
+      { name: "zeplin", url: require("Assets/zeplin.png") },
+    ],
+    description: [
+      '"CSS 변수를 사용했으며, 반응형으로 구현했습니다.(PC/Tablet/Mobile).',
+      'Vanilla.JS만을 사용하여 Javascript에 대한 이해도를 높였습니다."',
+    ],
+    challenges: [
+      "Responsive-web (Media query)",
+      "Vanilla JS",
+      "Swiper.js Api",
+      "Animation with Scroll events",
+      "DOM event 감지 (MutationObserver)",
+      "Zeplin 활용",
+    ],
+    browsers: ["Chrome", "Firefox", "Safari", "..."],
+    period: "2021.03 - 2021.04",
+  };
 
-        ],
-        challenges : [
-            "Responsive-web (Media query)",
-            "Vanilla JS",
-            "Swiper.js Api",
-            "Animation with Scroll events",
-            "DOM event 감지 (MutationObserver)",
-            "Zeplin 활용"
-        ],
-        browsers: [
-            "Chrome",
-            "Firefox",
-            "Safari",
-            "..."
-        ],
-        period: "2021.03 - 2021.04", 
-    }
-    
-    const styles = {
-        shadowColor: "#8090c4",
-        bgColor: "#becad1",
-        fontColor1: "#101f52",
-        fontColor2: "#111111",
-    }
+  const styles = {
+    shadowColor: "#8090c4",
+    bgColor: "#becad1",
+    fontColor1: "#101f52",
+    fontColor2: "#111111",
+  };
 
-    return <DetailFrame data = {data} styles = {styles}/>
-}
+  return <DetailFrame data={data} styles={styles} />;
+};
 
 export default Tada;
