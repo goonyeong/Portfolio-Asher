86bc8620eba301215c1fe78cba920fd1ff242523
asher <greatasher7@gmail.com>
2022-03-30 15:06:58 +0900

    add version

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,22 @@
 import "Components/Style/Header.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-
-    return(
-        <>
-            <section className="headerContainer">
-                <h1 className="greeting">Hello, I'm a <strong>Front-end developer</strong></h1>
-                <div className="viewMoreBtn">
-                    <strong>scroll to view more </strong>
-                    <FontAwesomeIcon icon={faChevronDown} className="iconArrowDown" />
-                </div>
-            </section>
-        </>
-    );
-}
+  return (
+    <>
+      <section className="headerContainer">
+        <h1 className="greeting">
+          Hello, I'm a <strong>Front-end developer</strong>
+          <span className="version">2021 / 06 ver.</span>
+        </h1>
+        <div className="viewMoreBtn">
+          <strong>scroll to view more </strong>
+          <FontAwesomeIcon icon={faChevronDown} className="iconArrowDown" />
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Header;
